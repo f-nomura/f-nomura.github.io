3a910b7451f6aa85e2c945942bf7d7333ed81e49
Fukumi Nomura <133613166+Fukumi-Nomura@users.noreply.github.com>
2026-04-11 16:23:38 +0900

    change config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'f-nomura',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://f-nomura.github.io' }],
 			sidebar: [
 				{
 					label: 'Guides',
