5960feb0a953542814aab96f7f5c7b2493e1cf0d
Fukumi Nomura <133613166+Fukumi-Nomura@users.noreply.github.com>
2026-04-12 16:32:30 +0900

    change folder_name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://f-nomura.github.io',
 	integrations: [
 		starlight({
 			title: 'f-nomura',
@@ -11,7 +12,7 @@ export default defineConfig({
 			sidebar: [
 				{
 					label: 'Documents',
-					autogenerate: { directory: 'documents' }, 
+					autogenerate: { directory: 'documents' },
 				},
 				{
 					label: 'Reference',
